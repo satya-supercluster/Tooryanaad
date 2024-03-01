1fe6cd9d4b87ab0e14e52ce7dd575425dbb6e92d
Satyam Gupta <satya.supercluster@gmail.com>
2024-03-01 19:50:15 +0530

    animation for dropdown

--- frontend/src/components/Header/DropDownMenu.jsx
+++ frontend/src/components/Header/DropDownMenu.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { motion } from "framer-motion"
+import { motion,AnimatePresence } from "framer-motion"
 import Hamburger from 'hamburger-react'
 const variants = {
   open: { opacity: 1, x: 0 },
@@ -7,7 +7,7 @@ const variants = {
 }
 const DropDownMenu = () => {
   const [dropDown,setDropDown]=useState(false);
-  
+
   return (
     <div 
     className='w-[90%] justify-center items-between sm:hidden flex flex-col gap-0.5'
@@ -19,8 +19,8 @@ const DropDownMenu = () => {
           onClick={() => setDropDown(!dropDown)}
           className='flex justify-between items-center bg-black p-3 rounded-lg'
           >
-              <a className='px-4 font-semibold text-lg'>तूर्यनाद समिति</a>
-              <Hamburger direction='right' toggled={dropDown} toggle={setDropDown} className=' items-center justify-center flex'></Hamburger>
+              <div className='pl-4 font-semibold text-lg'>तूर्यनाद समिति</div>
+              <Hamburger direction='right' toggled={dropDown} toggle={setDropDown} size={25} className=' items-center justify-center flex'></Hamburger>
         </motion.div>
 
 
@@ -29,12 +29,51 @@ const DropDownMenu = () => {
         {
         (dropDown===true)&&
         <div 
-        className='flex flex-col gap-1 items-center bg-black p-4 rounded-lg min-[480px]:w-1/2 min-[480px]:ml-auto'
+        className='flex flex-col items-center min-[480px]:w-1/2 min-[480px]:ml-auto'
         >
-            <a href="" className=' hover:scale-110'>मुख्य पृष्ठ</a>
-            <a href="" className=' hover:scale-110'>अतिथि</a>
-            <a href="" className=' hover:scale-110'>सदस्य</a>
-            <a href="" className=' hover:scale-110'>वीथिका</a>
+            <AnimatePresence>
+            <motion.div
+              className='w-full'
+              initial={{opacity:0,x:-150}}
+              animate={{opacity:1,x:0}}
+              exit={{opacity:0,x:150}}
+            >
+             <a href="" className='bg-black w-full flex justify-center items-center text-center py-2 rounded-t-lg hover:scale-110'>मुख्य पृष्ठ</a>
+            </motion.div>
+            </AnimatePresence>
+
+            <AnimatePresence>
+            <motion.div
+              className='w-full'
+              initial={{opacity:0,x:150}}
+              animate={{opacity:1,x:0}}
+              exit={{opacity:0,x:-150}}
+            >
+            <a href="" className='bg-black w-full flex justify-center items-center text-center py-2 hover:scale-110'>अतिथि</a>
+            </motion.div>
+            </AnimatePresence>
+
+            <AnimatePresence>
+            <motion.div
+              className='w-full'
+              initial={{opacity:0,x:-150}}
+              animate={{opacity:1,x:0}}
+              exit={{opacity:0,x:150}}
+            >
+            <a href="" className='bg-black w-full flex justify-center items-center text-center py-2 hover:scale-110'>सदस्य</a>
+            </motion.div>
+            </AnimatePresence>
+
+            <AnimatePresence>
+            <motion.div
+              className='w-full'
+              initial={{opacity:0,x:150}}
+              animate={{opacity:1,x:0}}
+              exit={{opacity:0,x:-150}}
+            >
+            <a href="" className='bg-black w-full flex justify-center items-center text-center py-2 rounded-b-lg hover:scale-110'>वीथिका</a>
+            </motion.div>
+            </AnimatePresence>
         </div>
         }  
 
