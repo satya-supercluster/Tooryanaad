81aafae52874f4342e5da8ebba4aa8a2d789df38
satya-supercluster <satya.supercluster@gmail.com>
2024-06-08 15:47:59 +0530

    revert

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
     backgroundImage: isMobile
       ? `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9)),url('/main.png')`
       : `linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.8)), url('/main.png')`,
-    backgroundSize: isMobile?"auto":"cover",
+    backgroundSize:"cover",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     backgroundAttachment: "fixed",
