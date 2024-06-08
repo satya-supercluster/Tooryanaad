ff5a128f94a5120af9e284351822bb6930e1a1cf
satya-supercluster <satya.supercluster@gmail.com>
2024-06-08 13:54:39 +0530

    bug-fixes

--- frontend/src/components/IconBar/IconBarSm.jsx
+++ frontend/src/components/IconBar/IconBarSm.jsx
@@ -14,35 +14,35 @@ const IconBarSm = () => {
         <a href="https://www.facebook.com/651896654877454" target="_blank">
           <FontAwesomeIcon
             icon={faFacebook}
-            className="px-2 py-2 bg-white text-black rounded-lg"
+            className="px-3 py-3 text-lg bg-white text-black rounded-lg"
           />
           {/* <i className="fa fa-facebook"></i> */}
         </a>
         <a href="https://twitter.com/tooryanaad_nitb?t=W8w1LQTbJ5UI_iC-UwUYzw&s=08">
           <FontAwesomeIcon
             icon={faTwitter}
-            className="px-2 py-2 bg-white text-black rounded-lg"
+            className="px-3 py-3 text-lg bg-white text-black rounded-lg"
           />
           {/* <i className="fa fa-twitter"></i> */}
         </a>
         <a href="https://instagram.com/tooryanaad?igshid=YmMyMTA2M2Y=">
           <FontAwesomeIcon
             icon={faInstagram}
-            className="px-2 py-2 bg-white text-black rounded-lg"
+            className="px-3 py-3 text-lg bg-white text-black rounded-lg"
           />
           {/* <i className="fa fa-instagram"></i> */}
         </a>
         <a href="https://www.linkedin.com/in/tooryanaad-nit-bhopal-66ab49151">
           <FontAwesomeIcon
             icon={faLinkedin}
-            className="px-2 py-2 bg-white text-black rounded-lg"
+            className="px-3 py-3 text-lg bg-white text-black rounded-lg"
           />
           {/* <i className="fa fa-linkedin"></i> */}
         </a>
         <a href="https://youtube.com/@TooryanaadNITBHOPAL">
           <FontAwesomeIcon
             icon={faYoutube}
-            className="px-2 py-2 bg-white text-black rounded-lg"
+            className="px-3 py-3 text-lg bg-white text-black rounded-lg"
           />
           {/* <i className="fa fa-youtube"></i> */}
         </a>
