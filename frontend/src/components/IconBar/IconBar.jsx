7da1171647d242a9f7059d7fc30a80ab243a4233
satya-supercluster <satya.supercluster@gmail.com>
2024-07-01 20:48:05 +0530

    transition

--- frontend/src/components/IconBar/IconBar.jsx
+++ frontend/src/components/IconBar/IconBar.jsx
@@ -10,65 +10,66 @@ import {
 import { motion } from "framer-motion";
 const IconBar = () => {
     return (
-      <motion.div
-        initial={{ y: 100 }}
-        animate={{ y: -113 }}
-        transition={{ duration: 0.6, ease: "easeOut" }}
-        className="max-sm:hidden fixed top-1/2 -translate-y-1/2 z-10 bg-[rgba(30,30,30)] shadow-[0_1px_5px_3px_rgba(0,0,0,0.6)] py-5 rounded-r-3xl"
-      >
-        <div className="grid grid-cols-1 gap-1 w-10">
-          <a
-            href="https://www.facebook.com/651896654877454"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faFacebook}
-              className="w-full  py-1 text-blue-500 hover:text-blue-600 bg-[rgba(30,30,30)] "
-            />
-          </a>
-          <a
-            href="https://instagram.com/tooryanaad?igshid=YmMyMTA2M2Y="
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faInstagram}
-              className="w-full  py-1 text-pink-500 hover:text-pink-600 bg-[rgba(30,30,30)] "
-            />
-          </a>
-          <a
-            href="https://twitter.com/tooryanaad_nitb?t=W8w1LQTbJ5UI_iC-UwUYzw&s=08"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faTwitter}
-              className="w-full  py-1 text-blue-500 hover:text-blue-600 bg-[rgba(30,30,30)] "
-            />
-          </a>
-          <a
-            href="https://youtube.com/@TooryanaadNITBHOPAL"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faYoutube}
-              className="w-full  py-1 text-red-500 hover:text-red-600 bg-[rgba(30,30,30)] "
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/tooryanaad-nit-bhopal-66ab49151"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faLinkedin}
-              className="w-full  py-1 text-blue-500 hover:text-blue-600 bg-[rgba(30,30,30)] "
-            />
-          </a>
-        </div>
-      </motion.div>
+      <div className="max-sm:hidden fixed top-1/2 -translate-y-1/2 z-10 ">
+        <motion.div
+          initial={{ x: -50 }}
+          animate={{ x: 0 }}
+          transition={{duration:0.5,ease:"easeInOut"}}
+          className="bg-[rgba(30,30,30)] shadow-[0_1px_5px_3px_rgba(0,0,0,0.6)] py-5 rounded-r-3xl">
+          <div className="grid grid-cols-1 gap-1 w-10">
+            <a
+              href="https://www.facebook.com/651896654877454"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faFacebook}
+                className="w-full  py-1 text-blue-500 hover:text-blue-600 bg-[rgba(30,30,30)] "
+              />
+            </a>
+            <a
+              href="https://instagram.com/tooryanaad?igshid=YmMyMTA2M2Y="
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="w-full  py-1 text-pink-500 hover:text-pink-600 bg-[rgba(30,30,30)] "
+              />
+            </a>
+            <a
+              href="https://twitter.com/tooryanaad_nitb?t=W8w1LQTbJ5UI_iC-UwUYzw&s=08"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faTwitter}
+                className="w-full  py-1 text-blue-500 hover:text-blue-600 bg-[rgba(30,30,30)] "
+              />
+            </a>
+            <a
+              href="https://youtube.com/@TooryanaadNITBHOPAL"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faYoutube}
+                className="w-full  py-1 text-red-500 hover:text-red-600 bg-[rgba(30,30,30)] "
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/tooryanaad-nit-bhopal-66ab49151"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                className="w-full  py-1 text-blue-500 hover:text-blue-600 bg-[rgba(30,30,30)] "
+              />
+            </a>
+          </div>
+        </motion.div>
+      </div>
     );
 };
 
