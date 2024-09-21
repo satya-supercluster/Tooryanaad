e4d5d6f13626d8af719ae8b9dbc3af7118819ea7
Aditya Kumar <97442241+iamaditya21@users.noreply.github.com>
2024-09-21 12:54:20 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -21,6 +21,7 @@ app.use(
       "https://tooryanaad.netlify.app",
       "https://tooryanaad-info.netlify.app",
       "https://tooryanaad-backend-muz0.onrender.com",
+      "https://tooryanaad-23.onrender.com/"
     ],
     credentials: true, //access-control-allow-credentials:true
   })
