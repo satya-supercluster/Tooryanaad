ee8ad00ed8c1a945259a3ce4160b63c08626647a
satya-supercluster <satya.supercluster@gmail.com>
2025-03-30 20:14:02 +0530

    Add registration date field to registration25 model

--- backend/api/model/registration25.js
+++ backend/api/model/registration25.js
@@ -4,36 +4,41 @@ const mongoose = require("mongoose");
 const regSchema = {
   token: {
     type: String,
-    required: true
+    required: true,
   },
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: true
+    required: true,
   },
   year: {
     type: String,
-    required: true
+    required: true,
   },
   contact: {
     type: String,
-    required: true
+    required: true,
   },
   scholar: {
     type: String,
-    required: true
+    required: true,
   },
   branch: {
     type: String,
-    required: true
+    required: true,
   },
   vertical: {
     type: [String],
-    required: true
-  }
+    required: true,
+  },
+  Date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
 };
 
 const Registration = mongoose.model('Ahvaan25', regSchema);
