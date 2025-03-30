e242500b9b62d9d09e7e377d94f5e6558435b831
satya-supercluster <satya.supercluster@gmail.com>
2025-03-30 20:23:21 +0530

    Refactor registration25 model to use mongoose.Schema and add timestamps

--- backend/api/model/registration25.js
+++ backend/api/model/registration25.js
@@ -1,45 +1,42 @@
-
 const mongoose = require("mongoose");
 
-const regSchema = {
-  token: {
-    type: String,
-    required: true,
+const regSchema = new mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    contact: {
+      type: String,
+      required: true,
+    },
+    scholar: {
+      type: String,
+      required: true,
+    },
+    branch: {
+      type: String,
+      required: true,
+    },
+    vertical: {
+      type: [String],
+      required: true,
+    },
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: String,
-    required: true,
-  },
-  contact: {
-    type: String,
-    required: true,
-  },
-  scholar: {
-    type: String,
-    required: true,
-  },
-  branch: {
-    type: String,
-    required: true,
-  },
-  vertical: {
-    type: [String],
-    required: true,
-  },
-  Date: {
-    type: Date,
-    default: Date.now,
-    required: true,
-  },
-};
+  { timestamps: true }
+);
 
-const Registration = mongoose.model('Ahvaan25', regSchema);
+const Registration = mongoose.model("Ahvaan25", regSchema);
 module.exports = Registration;
