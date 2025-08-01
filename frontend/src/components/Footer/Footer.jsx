b995a28cb08b68a64971d4631e09d44ce9d16047
ShivamxCj <shivam.chatterjee.30jun@gmail.com>
2025-08-02 00:32:34 +0530

    changed phone number

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -66,7 +66,7 @@ export default function Footer() {
               icon={faPhoneAlt}
               className="text-sm bg-gray-900 rounded-full p-2"
             />
-            <p className="inline text-sm ml-4">(+91) 9588271775</p>
+            <p className="inline text-sm ml-4">(+91) 9404429037</p>
           </div>
 
           <div className="mb-4 flex items-center">
