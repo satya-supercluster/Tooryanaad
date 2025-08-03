2010cdb31f90e9f4b3812a96f0d754591c0c2450
ShivamxCj <shivam.chatterjee.30jun@gmail.com>
2025-08-03 15:49:38 +0530

    updated the about part and yt, atwitter, insta followers

--- frontend/src/components/About/Counter.jsx
+++ frontend/src/components/About/Counter.jsx
@@ -4,10 +4,10 @@ import { useInView } from "react-intersection-observer";
 
 const Counter = () => {
   const counters = [
-    { label: "इंस्टाग्राम", value: 3900 },
+    { label: "इंस्टाग्राम", value: 4800 },
     { label: "फेसबुक", value: 20000 },
-    { label: "यूट्यूब", value: 3500 },
-    { label: "ट्विटर", value: 800 },
+    { label: "यूट्यूब", value: 3600 },
+    { label: "ट्विटर", value: 830 },
   ];
 
   const [ref, inView] = useInView({
