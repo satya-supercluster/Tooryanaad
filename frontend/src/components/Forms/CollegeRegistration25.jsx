cbf691142fe80fa00e650cb31c8fe2f559f60805
kashish25052004 <kashishjaiswal25052004@gmail.com>
2026-02-28 00:10:06 +0530

    Fix sponsorship

--- frontend/src/components/Forms/CollegeRegistration25.jsx
+++ frontend/src/components/Forms/CollegeRegistration25.jsx
@@ -36,7 +36,7 @@ export default function CollegeRegistration25() {
         "वेबसाइट बनाने, विकसित करने और कार्यशील बनाने का कार्य करता है।",
     },
     {
-      id: "प्रयोजन प्रबन्धक",
+      id: "प्रायोजन प्रबन्धक",
       translatedName: "Sponsorship Executive",
       description:
         "प्रायोजकों से समन्वय कर कार्यक्रमों हेतु वित्तीय एवं संसाधन सहयोग सुनिश्चित करता है।"
