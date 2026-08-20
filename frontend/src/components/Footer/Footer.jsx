628ca2de07b17e1ef4ed9d5b3059c79f5ee96a3d
kashish25052004 <149101294+kashish25052004@users.noreply.github.com>
2026-08-21 01:34:33 +0530

    Update contact phone number in Footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -66,7 +66,7 @@ export default function Footer() {
               icon={faPhoneAlt}
               className="text-sm bg-gray-900 rounded-full p-2"
             />
-            <p className="inline text-sm ml-4">(+91) 9404429037</p>
+            <p className="inline text-sm ml-4">(+91) 6261988045</p>
           </div>
 
           <div className="mb-4 flex items-center">
